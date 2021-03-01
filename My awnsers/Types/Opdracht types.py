b05eb26d2368c015dd3d0aed640b98554898bcaf
LFGaming <38732192+LFGaming@users.noreply.github.com>
2021-03-01 17:48:40 +0100

    Update Opdracht types.py

--- My awnsers/Types/Opdracht types.py
+++ My awnsers/Types/Opdracht types.py
@@ -1,3 +1,4 @@
+#%%
 # Met de type([variabele]) functie kun je het type van een variabele achterhalen. Welke types hebben de volgende variabelen? Tip: Probeer het eerst zelf te bedenken, voordat je het met de functie controleert
 #Voorbeeld:
 
@@ -10,34 +11,34 @@ b = 'Hello World' # string (str)
 c = 2.0 #floats (float)
 d = True # Boolean (bool)
 
-e = a == b
-f = a > c
+e = a == b #bool
+f = a > c #bool
 
 # Doe hetzelfde voor de volgende expressies, maar probeer ook te bedenken wat de resultaten van de expressies zouden moeten zijn. Wederom; probeer dit eerst zelf te bedenken voordat je het laat berekenen door Python
 
-g = 4 + 23
-h = 12 + 0.5
-i = 1.0 - 1.0
+g = 4 + 23 #int
+h = 12 + 0.5 #float
+i = 1.0 - 1.0 #float
 
-j = 2 * 5
-k = 2.0 * 10
-l = k - 1.0
+j = 2 * 5 #int
+k = 2.0 * 10 #float
+l = k - 1.0 #float
 
-m = 10 / 2
-n = 11 / 4
-o = 11 // 4
-p = 8**2
-q = 2**8
-r = 15 % 3
-s = 3 % 15
-t = 20 % 8
+m = 10 / 2 #float
+n = 11 / 4 #float
+o = 11 // 4 #int
+p = 8**2 #int
+q = 2**8 #int
+r = 15 % 3 #int
+s = 3 % 15 #int
+t = 20 % 8 #int
 
-u = 24
-v = 12
-u = v = 42
-w = u != v
+u = 24 #int
+v = 12 #int
+u = v = 42 #
+w = u != v #bool
 
-x = 2+3*9+3**3/6
+x = 2+3*9+3**3/6 #float
 
 
 # Als we nu de volgende regels 'uitcommenten' ('# ' voor de regel verwijderen) wordt ook deze code uitgevoerd.
@@ -45,3 +46,4 @@ x = 2+3*9+3**3/6
 # z = 'Hallo Wereld'
 # y = z
 # Van welk type zijn y en z dan? 
+# %%
