86520a49a0dd76951d46eed5eb43872b3a5aaaa9
LFGaming <38732192+LFGaming@users.noreply.github.com>
2021-03-08 17:36:37 +0100

    lists

---
+++ My awnsers/lists/main.py
@@ -0,0 +1,45 @@
+#%%
+from hidden import onze_lijst
+
+# > Bereken de som van de lijst genaamd 'onze_lijst'.
+# Deze lijst is gevuld met unieke, willekeurige getallen
+print(type(onze_lijst))
+
+# > Bereken ook het gemiddelde van deze lijst
+
+average = sum(onze_lijst) / len(onze_lijst)
+
+print(average)
+
+# > Bekijk of het element op index 5 van deze lijst groter is dan het gemiddelde
+
+
+
+# > Wat is het laatste element in de lijst? Bekijk dit door naar het laatste element in de lijst te kijken zonder de lengte van de lijst te gebruiken.
+
+
+# > Wat is het grootste getal in de lijst? Vraag de index op van dit getal
+
+# > Plaats voor en na dit getal een 0.
+# Dus als 10 het grootste getal is wordt de lijst [0, 10, 0]
+
+# > Zoek het grooste nummer deze keer in de lijst door de lijst te sorteren en naar het laatste element te kijken. Vergelijk dit met het grootste nummer dat je eerst vond.
+
+# Draai de lijst om, zodat nu het kleinste getal achteraan staat. Controleer dit door te kijken of het eerste element nog steeds het grootste element is.
+
+
+# > Maak nu de lijst leeg
+
+#> Maak een string en vul deze met minimaal 20 letters (hoeft geen bestaand woord o.i.d. te zijn)
+# > Vraag met input om een letter en tel hoe vaak die letter in de lijst staat
+
+# Hieronder een lijst met wachtwoorden:
+wachtwoorden = ['wachtwoord', '1234', '0000', 'mijn_verjaardag','naam_van_huisdier', 'qwerty', 'admin', 'wachtw00rd']
+
+# > Vraag een gebruiker om zijn gebruikersnummer (dat is de index in de lijst, dus gebruiker 0 heeft als wachtwoord 'wachtwoord')
+# > Vraag een gebruiker om zijn wachtwoord
+# > Kijk of het wachtwoord klopt
+# > Laat de gebruiker het wachtwoord aanpassen
+# > Sla deze aanpassing op de goede plek in de lijst op
+# > Controleer of het wachtwoord veranderd is
+# %%
